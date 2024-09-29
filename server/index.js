1d219954b05f77eea14cfa16b41f8958ee896b99
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-09-29 13:17:04 -0600

    add socket disconnected user

--- server/index.js
+++ server/index.js
@@ -1,25 +1,27 @@
-import express from "express"
-import logger from "morgan"
-import { Server } from "socket.io"
-import { createServer } from "node:http"
+import express from "express";
+import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT ?? 3000;
 
-const app = express()
-const server = createServer(app)
-const io=new Server(server)
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-io.on("connection",()=>{
-    console.log("a user connected")    
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
+app.use(logger("dev"));
 
-app.use(logger("dev"))
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
 
-app.get("/",(req,res)=>{
-    res.sendFile(process.cwd()+"/client/index.html")
-})
-
-server.listen(port,()=>{
-    console.log(`Server running on port http://localhost:${port}`) 
-})
+server.listen(port, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
