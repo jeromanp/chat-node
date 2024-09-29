d00060e02b5334cab44ec98f50e79203f6577284
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-09-28 23:22:59 -0600

    listen server in port  3000

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express"
 
-const port = processs.env.PORT ?? 3000
+const port = process.env.PORT ?? 3000
 
 const app = express()
 
 app.get("/",(req,res)=>{
     res.send("<h1>Hola mundo</h1>")
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port http://localhost:${port}`) 
 })
