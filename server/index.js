436a3cc3031b8ec4e102623fc50e79a759adf46c
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-09-30 23:47:20 -0600

    save message in db

--- server/index.js
+++ server/index.js
@@ -29,16 +29,23 @@ await db.execute(`
   )
 `);
 
-
-
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
-  socket.on("chat message", (msg) => {
-    // console.log("message: " + msg);
-    io.emit("chat message", msg);
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.execute({
+        sql: "INSERT INTO messages (content) VALUES (:content)",
+        args: { content: msg },
+      });
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+    io.emit("chat message", msg, result.lastInsertRowid.toString());
   });
 });
 
